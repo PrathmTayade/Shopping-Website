6f095ea237b0954c95a8ca6a9939c3c3325b4f80
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-03-15 20:27:07 +0530

    something diff

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -5,20 +5,16 @@ export default function CheckoutPage() {
   const { cartItem } = useContext(StateContext);
   const [quantity, setQuantity] = useState();
 
-  const quantityHandler = (e) => {
-    setQuantity([...quantity, e]);
-    console.log(quantity);
-  };
   const [amount, setAmount] = useState();
 
-  useEffect(() => {
-    const result = cartItem.reduce(
-      (total, currentValue) =>
-        (total = total + currentValue.quantity * currentValue.price),
-      0
-    );
-    return setAmount(result), console.log(amount);
-  }, [cartItem]);
+  // useEffect(() => {
+  //   const result = cartItem.reduce(
+  //     (total, currentValue) =>
+  //       (total = total + currentValue.quantity * currentValue.price),
+  //     0
+  //   );
+  //   return setAmount(result), console.log(amount);
+  // }, []);
 
   const total = () => {
     const result = cartItem.reduce(
@@ -28,51 +24,29 @@ export default function CheckoutPage() {
     );
     return result;
   };
+
   return (
     <div className="container flex w-screen flex-1 flex-col bg-slate-50 dark:bg-slate-900   ">
       <div className=" w-full flex-auto overflow-auto p-5 ">
-        <div className="mx-auto border  shadow-md ">
-          <table className="  mx-auto w-full table-auto  text-center   text-sm text-gray-500 dark:text-gray-400">
-            <thead className="bg-gray-50  uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-              <tr className=" ">
-                <th className=" py-3 ">Item</th>
-                {/* <th className=" py-3 ">Quantity</th> */}
-                <th className=" py-3 ">Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cartItem.map((items) => (
-                <tr
-                  key={items.id}
-                  className=" border-b bg-white dark:border-gray-700 dark:bg-gray-800"
-                >
-                  <td className=" px-6 py-4">
-                    <div className=" flex  ">
-                      <div className="  w-14 ">
-                        <img className="w-full " src={items.image} alt="" />
-                      </div>
-                      <div className="hidden px-3 py-2 text-lg font-bold md:block ">
-                        {items.title}
-                      </div>
-                    </div>
-                  </td>
-                  {/* <td className=" px-6 py-4 ">
-                    <div className="flex flex-col  shadow-md  transition  duration-150 ease-in-out md:inline-flex  md:flex-row ">
-                      <NoofItems item={items} />
-                    </div>
-                  </td> */}
-                  <td className=" px-6 py-4">{items.price * items.quantity}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="grid-row-auto mx-auto grid border shadow-md ">
+          <div className="flex justify-around border bg-slate-100 p-2 font-bold uppercase   text-slate-500 shadow-sm dark:bg-slate-900 ">
+            <div>Item</div>
+            <div>Quantity</div>
+            <div>price</div>
+          </div>
+
+          {cartItem.map((item) => (
+            <div key={item.id} className="flex justify-between">
+              <CartItem item={item} changehandler={setAmount} />
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex   w-full justify-evenly  px-5  text-3xl    ">
         <div className=" font-bold  ">Total Price: </div>
-        {/* <div className="">
+        <div className="">
           <button onClick={() => console.log(total())}>total</button>
-        </div> */}
+        </div>
         <div className=" md:px-10 ">₹ {amount} </div>
       </div>
       <div className=" flex  items-center justify-center rounded-b border-t border-solid border-slate-200 p-6 md:justify-end">
@@ -90,7 +64,7 @@ export default function CheckoutPage() {
   );
 }
 
-function NoofItems({ item }) {
+function CartItem({ item, changehandler }) {
   const [quantity, setQuantity] = useState(1);
 
   function incrementItems(item) {
@@ -110,25 +84,47 @@ function NoofItems({ item }) {
     }
   }
   return (
-    <div>
-      <div>
-        <button
-          type="button"
-          onClick={() => incrementItems(item)}
-          className="inline-block rounded-t bg-sky-400 px-3 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-700 md:rounded-l md:rounded-tr-none md:px-6"
-        >
-          +
-        </button>
-        <div className="inline-block bg-sky-400 px-3  pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-700 md:px-6">
-          {quantity}
+    <div className=" mx-auto w-full px-4 py-2    dark:text-gray-400">
+      <div className="">
+        <div className=" flex  ">
+          <div className="  w-14 ">
+            <img className="w-full " src={item.image} alt="" />
+          </div>
+          <div className="hidden px-3 py-2 text-lg font-bold md:block ">
+            {item.title}
+          </div>
         </div>
-        <button
-          type="button"
-          onClick={() => decrementItems(item)}
-          className="inline-block rounded-b bg-sky-400 px-3 pt-2.5  pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0  active:bg-sky-700 md:rounded-r md:rounded-bl-none md:px-6"
-        >
-          -
-        </button>
+      </div>
+      <div className=" px-6 py-4 ">
+        <div className="flex flex-col  shadow-md  transition  duration-150 ease-in-out md:inline-flex  md:flex-row ">
+          <div>
+            <button
+              type="button"
+              onClick={() => incrementItems(item)}
+              className="inline-block rounded-t bg-sky-400 px-3 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-700 md:rounded-l md:rounded-tr-none md:px-6"
+            >
+              +
+            </button>
+            <div className="inline-block bg-sky-400 px-3  pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0 active:bg-sky-700 md:px-6">
+              {item.quantity}
+            </div>
+            <button
+              type="button"
+              onClick={() => decrementItems(item)}
+              className="inline-block rounded-b bg-sky-400 px-3 pt-2.5  pb-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-sky-600 focus:bg-sky-600 focus:outline-none focus:ring-0  active:bg-sky-700 md:rounded-r md:rounded-bl-none md:px-6"
+            >
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className=" px-6 py-4"
+        onChange={() => {
+          changehandler(item.price * quantity);
+        }}
+      >
+        {item.price * quantity}
       </div>
     </div>
   );
